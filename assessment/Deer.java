679661d9591326dfc3fa57d405823ef055c6cd76
Luka <odak.skywalker@gmail.cm>
2019-12-19 19:43:09 +0100

    Code from assessment test

---
+++ assessment/Deer.java
@@ -0,0 +1,16 @@
+public class Deer{
+	public Deer() { System.out.print("Deer");}
+	public Deer(int age) { System.out.print("DeerAge"); }
+	private boolean hasHorns() { return false; }
+
+	public static void main(String[] args){
+		Deer deer = new Reindeer(5);
+		System.out.println("," + deer.hasHorns());
+}
+}
+class Reindeer extends Deer{
+	public Reindeer() { System.out.print("New Reindeer"); }
+	//it is not overriden method as parent method is declared as private
+	public Reindeer(int age) { System.out.print("Reindeer"); }
+	public boolean hasHorns() { return true; }
+}
